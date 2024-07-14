7b77306f29694ef2f4c0d6448125a2006af44b63
Nathan Sudiara <nathansudiara@gmail.com>
2024-07-14 14:55:00 +0700

    refactor: Update image paths in Footer component

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,11 @@
+import logo from "../assets/logoHeader.svg";
+
 const Footer = () => {
   return (
     <footer className="flex flex-row h-[100] w-full justify-between items-center px-8">
       <div className="justify-center items-start">
         <a href="#home">
-          <img src="..\src\assets\logoHeader.svg" alt="logo" />
+          <img src={logo} alt="logo" />
         </a>
       </div>
       <div className="flex flex-col justify-center items-end gap-3 sora-200 text-white text-base">
