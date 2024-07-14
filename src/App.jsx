fc7f7e7388db2ac4998010d8e734913b69a877ff
Nathan Sudiara <nathansudiara@gmail.com>
2024-07-14 18:33:39 +0700

    refactor: Update image paths in Footer, Nav, and Hero components

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,33 @@ import Footer from "./sections/Footer";
 
 const App = () => (
   <main id="home" className="w-full">
-    <Nav></Nav>
-    <section className="px-20 py-16 mt-7">
-      <Hero />
-    </section>
-    <section id="skills" className="px-20 py-16">
-      <Skills />
-    </section>
-    <section id="experience" className="bg-black px-20 py-16">
-      <Experience />
-    </section>
-    <section id="about" className="px-20 py-16">
-      <About />
-    </section>
-    <section id="project" className="bg-black px-20 py-16">
-      <Project />
-    </section>
-    <section id="contact" className="px-20 py-16">
-      <Contact />
-    </section>
-    <section className="px-20 py-6 bg-black">
-      <Footer />
-    </section>
+    <div className="max-lg:hidden">
+      <Nav></Nav>
+      <section className="px-20 py-16 mt-7 max-sm:px-4 max-sm:py-10 max-sm:mt-0">
+        <Hero />
+      </section>
+      <section id="skills" className="px-20 py-16">
+        <Skills />
+      </section>
+      <section id="experience" className="bg-black px-20 py-16">
+        <Experience />
+      </section>
+      <section id="about" className="px-20 py-16">
+        <About />
+      </section>
+      <section id="project" className="bg-black px-20 py-16">
+        <Project />
+      </section>
+      <section id="contact" className="px-20 py-16">
+        <Contact />
+      </section>
+      <section className="px-20 py-6 bg-black">
+        <Footer />
+      </section>
+    </div>
+    <div className="hidden max-lg:block h-screen w-screen sora-800 text-4xl">
+      <h1 className="flex h-screen w-screen justify-center items-center p-10">Mohon buka dengan laptop ya ges aokwokwok</h1>
+    </div>
   </main>
 );
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,32 @@
 // rafce
 import logoHeader from "../assets/logoHeader2.svg";
+import hamburger from "../assets/hamburger.svg";
 
 const Nav = () => {
   return (
-    <header className="px-20 py-6 sticky top-0 backdrop-blur drop-shadow-lg bg-white/60 w-auto z-50">
+    <header className="px-20 py-6 sticky top-0 backdrop-blur drop-shadow-lg bg-white/60 w-auto z-50 max-lg:p-4">
       <nav className="px-8 flex justify-between items-center flex-row">
         <div className="justify-start">
           <a href="#home">
             <img src={logoHeader} alt="logo" />
           </a>
         </div>
-        <div className="justify-center space-x-8 sora-600 text-xl">
+        <div className="justify-center space-x-8 sora-600 text-xl max-lg:hidden">
           <a className="hover:text-gray-500" href="#about">About Me</a>
           <a className="hover:text-gray-500" href="#skills">Skills</a>
           <a className="hover:text-gray-500" href="#project">Projects</a>
           <a className="hover:text-gray-500" href="#contact">Contact Me</a>
         </div>
-        <div className="justify-end">
+        <div className="justify-end max-lg:hidden">
           <a
             href="https://youtu.be/xvFZjo5PgG0?si=yKEyHpcwxQhWjwD0"
             className="w-36 h-14 px-5 py-4 rounded bg-black justify-center items-center flex text-white text-xl sora-600 hover:bg-white cursor-pointer transition-all duration-300 ease-in-out hover:text-black hover:border-solid hover:border-2 hover:border-black"
           >
             rawrrr
           </a>
+        </div>
+        <div className="justify-end hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" className="w-10 h-10"/>
         </div>
       </nav>
     </header>
